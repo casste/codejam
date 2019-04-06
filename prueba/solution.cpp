9880a0242206c5cf705601f084f1fecd916a896a
Alejandro Castellanos <acdelatorre@iusa.com.mx>
2019-04-06 02:42:23 -0600

    codejam

--- prueba/solution.cpp
+++ prueba/solution.cpp
@@ -4,13 +4,67 @@ using namespace std;
 
 // Function to find all Permutations of a given string
 // containing all distinct characters
-void permutations(string str, int n, string res)
+string org;
+string solve;
+
+int permutations(string str, int n, string res, int N)
 {
+
     // base condition (only one character is left in the string)
     if (n == 1)
     {
-        cout << res + str << endl;
-        return;
+
+        string nueva=res+str;
+        bool marca=true;
+
+        int rutaorg[2*N-2][2];
+        rutaorg[0][0] = 0;
+        int suma=0;
+        for(int i=0;i<org.size();i++){
+            if(org[i]=='E'){
+                rutaorg[i][0]=suma;
+                suma++;
+                rutaorg[i][1]=suma;
+            }else{
+                rutaorg[i][0]=suma;
+                suma+=5;
+                rutaorg[i][1]=suma;
+            }
+            // cout<<rutaorg[i][0]<<','<<rutaorg[i][1]<<endl;
+        }
+
+
+        int rutanueva[2*N-2][2];
+        rutanueva[0][0] = 0;
+        suma=0;
+        for(int i=0;i<nueva.size();i++){
+            if(nueva[i]=='E'){
+                rutanueva[i][0]=suma;
+                suma++;
+                rutanueva[i][1]=suma;
+            }else{
+                rutanueva[i][0]=suma;
+                suma+=5;
+                rutanueva[i][1]=suma;
+            }
+            // cout<<rutanueva[i][0]<<','<<rutanueva[i][1]<<endl;
+        }
+
+        for(int i=0;i<nueva.size();i++){
+            for(int j=0;j<nueva.size();j++){
+                if(rutanueva[j][0]==rutaorg[i][0] && rutanueva[j][1]==rutaorg[i][1]){
+                    marca=false;
+                }
+            }
+        }
+
+        if( marca ){
+            solve=nueva;
+            return -1;
+        }
+        
+        // cout << marca + res + str << endl;        
+        return 0;
     }
     
     // process each character of the remaining string
@@ -18,108 +72,35 @@ void permutations(string str, int n, string res)
     {
         // push current character to the output string and recurse
         // for the remaining characters
-        permutations(str.substr(1), n - 1, res + str[0]);
+        if(permutations(str.substr(1), n - 1, res + str[0], N)){
+            return -1;
+        }
 
         // left rotate the string by 1 unit for next iteration
         // to right rotate the string use reverse iterator
         rotate(str.begin(), str.begin() + 1, str.end());
     }
+
+    return 0;
 }
 
 // Find all Permutations of a string
 int main()
 {
-    string str = "SE";
+    string str;
     string res;    // empty string
+    int T;
+    cin>>T;
+    for(int t=1;t<=T;t++) {
+        int N;
+        cin>>N;        
+        cin>>org;
+        str=org;
 
-    permutations(str, str.size(), res);
+        if(permutations(str, str.size(), res, N)){
+            cout<<"Case #"<<t<<": "<<solve<<endl;
+        }
+    }
 
     return 0;
 }
-
-
-
-// #include <iostream>
-
-// using namespace std;
-
-// int main() {
-//     int T;
-//     cin>>T;
-//     for(int t=1;t<=T;t++) {
-//         int N;
-//         cin>>N;
-
-//         // cout<<N<<endl;
-
-//         bool S[N][N], E[N][N];
-//         int r,c,p;
-//         char out[2*N-2];
-        
-//         for(r=0;r<N;r++){
-//             for(c=0;c<N;c++){
-//                 S[r][c]=1;
-//                 E[r][c]=1;
-//                 // cout<<S[r][c];
-//             }
-//             // cout<<endl;
-//         }        
-//         // cout<<endl;
-        
-//         r=c=0;
-//         for(p=0;p<(2*N-2);p++){
-//             char o;
-//             cin>>o;
-//             // cout<<o<<endl;
-//             if(o=='S') {
-//                 S[r][c]=0;
-//                 r++;
-//             }else{
-//                 E[r][c]=0;
-//                 c++;
-//             }
-//         }
-//         // for(r=0;r<N;r++){
-//         //     for(c=0;c<N;c++){
-//         //         cout<<S[r][c];
-//         //     }
-//         //     cout<<endl;
-//         // }
-//         // cout<<endl;
-        
-//         for(r=0;r<(2*N-2);r++) out[r]=0;
-
-//         r=c=p=0;
-//         if(E[r][c+1]){
-//             out[p]='E';
-//             c++;
-//         }else{
-//             out[p]='S';
-//             r++;
-//         }
-
-//         for(p=1;p<(2*N-2);p++)
-//         {
-//             if(out[p-1]=='E'){
-//                 if(S[r][c] && r<N){
-//                     out[p]='S';
-//                     r++;
-//                 }else{
-//                     out[p]='E';
-//                     c++;
-//                 }
-//             }else{
-//                 if(E[r][c] && c<N){
-//                     out[p]='E';
-//                     c++;
-//                 }else{
-//                     out[p]='S';
-//                     r++;
-//                 }
-//             }
-//         }
-//         out[2*N-2]=0;
-        
-//         cout << "Case #" << t << ": "<< out << endl;
-//     }
-// }
---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// #define DEBUG 1
+
+int main() {
+    int T;
+    cin >> T;
+#ifdef DEBUG
+    cout << T << endl;
+#endif
+  
+    for(int t=1;t<=T;t++) {
+
+        cout << "Case #" << t << ": "<< "?" << endl;
+    }
+}
